70233afccf025d2e1a26a8f47a5b3c2c69ce4ed2
Celso Mattheus Cantanhede Silva <02741207399@receita.intranet>
2019-08-08 18:05:31 -0300

    funÃção para extração de dados de unidades de atendimento, por testar

---
+++ 02_dados/01_scrapping_RFB/function extract table unid.r
@@ -0,0 +1,7 @@
+tabularUnid <- function(url){
+  remDr$navigate(url)
+  df <- readHTMLTable(htmlParse(remDr$getPageSource()[[1]], encoding = 'UTF-8')) [[1]] %>% 
+          spread(key = V1, value = V2) %>% 
+          select(Unidade, Estado, Cidade, Bairro, CEP, Logradouro, Atendimento, Titular, 'Telefone(s)', Observações)infoUnids <- c(infoUnids, dfUnid)
+  return(df)
+}
