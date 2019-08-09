86a71fdd9e35ab4689a38a8f36af5ee4eff8cbf8
Matth3us <celso.mattheus@gmail.com>
2019-08-08 23:24:57 -0300

    Reorganização de arquivos

--- 02_dados/01_scrapping_RFB/function extract table unid.r
+++ 02_dados/01_scrapping_RFB/function extract table unid.r
@@ -2,6 +2,6 @@ tabularUnid <- function(url){
   remDr$navigate(url)
   df <- readHTMLTable(htmlParse(remDr$getPageSource()[[1]], encoding = 'UTF-8')) [[1]] %>% 
           spread(key = V1, value = V2) %>% 
-          select(Unidade, Estado, Cidade, Bairro, CEP, Logradouro, Atendimento, Titular, 'Telefone(s)', Observações)infoUnids <- c(infoUnids, dfUnid)
+          select(Unidade, Estado, Cidade, Bairro, CEP, Logradouro, Atendimento, Titular, 'Telefone(s)', Observações)
   return(df)
 }
