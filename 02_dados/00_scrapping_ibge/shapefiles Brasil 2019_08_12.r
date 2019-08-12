4d9c381c88126abcc8f5b1c9e71c38609bdcce1e
Matth3us <celso.mattheus@gmail.com>
2019-08-12 08:53:44 -0300

    script para baixar geometrias vetoriais do Brasil, dos Estados e dos Municipios

---
+++ 02_dados/00_scrapping_ibge/shapefiles Brasil 2019_08_12.r
@@ -0,0 +1,9 @@
+library(geobr)
+library(sf)
+Brasil <- geobr::read_country(year = 2018)
+Estados <- geobr::read_state(code_state = "all", year=2018)
+Municipios <- geobr::read_municipality(code_muni = "all", year=2018)
+geoInfo <- list(brasil = Brasil, estados = Estados, municipios = Municipios)
+
+#teste de plotagem do mapa do Brasil
+plot(st_geometry(Brasil))
