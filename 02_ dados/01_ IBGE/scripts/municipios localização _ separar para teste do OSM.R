5bf24d885ef6864cc9b144c0488df5984f308a27
Matth3us <celso.mattheus@gmail.com>
2019-08-16 10:01:49 -0300

    Teste de tabela gerada por libary osmr

--- 02_ dados/01_ IBGE/scripts/municipios localização _ separar para teste do OSM.R
+++ 02_ dados/01_ IBGE/scripts/municipios localização _ separar para teste do OSM.R
@@ -7,12 +7,6 @@ municipios <- read_sf("./02_ dados/01_ IBGE/dados_brutos/BR_Localidades_2010_v1.
                 mutate(NM_UF = as.factor(NM_UF))
 
 #municipios para teste
-paraTestes <- municipios %>% filter(NM_MUNICIP %in% c("BRASÍLIA", "SÃO PAULO", "SÃO JOSÉ DOS CAMPOS", "SÃO LUÍS"))
+paraTestes <- municipios %>% filter(NM_MUNICIP %in% c("BRASÍLIA", "SÃO PAULO", "SÃO JOSÉ DOS CAMPOS", "SÃO LUÍS", "PARINTINS"))
 library(osrm)
-options(osrm.server = "http://127.0.0.1:9966/")
 testTable <- osrm::osrmTable(paraTestes, measure = c("duration", "distance"))
-
-saveRDS
-
-#sudo docker run -p 9966:9966 osrm/osrm-frontend
-#sudo xdg-open 'http://127.0.0.1:9966'
