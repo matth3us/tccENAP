3433ad4f368a9d038d8e0555ccc35d0a1f51b238
Celso Mattheus <rstudio@my-osrm.southamerica-east1-b.c.evaluation-tcc.internal>
2019-08-19 23:49:51 -0300

    otimizado o cdiógo de estimativa de distâncias

--- 02_ dados/03_ analise/1_tempo_entre_municipios.r
+++ 02_ dados/03_ analise/1_tempo_entre_municipios.r
@@ -1,9 +1,17 @@
+library(tidyverse)
 muni <- readRDS("./02_ dados/01_ IBGE/dados_modificados/locus_municipios.rds") %>% 
             select(CD_GEOCODM, NM_MUNICIP, NM_UF, LONG, LAT, ALT, geometry)
 
+
+#reescrever o código, para fazer de 100 em 100.
+
 inputMuni <- muni %>% select(CD_GEOCODM, geometry) %>% rename(cod_ibge = CD_GEOCODM)
+srcMuni <- inputMuni %>% slice(1:100)
+dstMuni <- inputMuni %>% slice(101:102)
+
+
 
 library(osrm)
-options(osrm.server: 'http://0.0.0.0:5000/')
-outputMuni <- osrmTable(loc = inputMuni, measure= c('duration', 'distance')) #duration in minutes; distance in meters
+options(osrm.server = 'http://0.0.0.0:5000/')
+outputMuni <- osrmTable(src=srcMuni, dst=dstMuni, measure= c('duration', 'distance')) #duration in minutes; distance in meters
 saveRDS(outputMuni, '1_tempo_entre_municipios.rds')
