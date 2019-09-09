edcaf2ecbd06f182dbda424d906bba517cd6d7ba
Celso Mattheus Cantanhede Silva <02741207399@receita.intranet>
2019-09-09 16:14:38 -0300

    concluida organização de viagens entre cidades e areas de ponderação

--- 02_ dados/03_ analise/1_4_juntar tabelas tempos municipios areas.R
+++ 02_ dados/03_ analise/1_4_juntar tabelas tempos municipios areas.R
@@ -8,17 +8,17 @@ travels <- readRDS('./02_ dados/03_ analise/1_tempo_municipios_areas.rds')
 area <- readRDS("./02_ dados/03_ analise/03_01_areasPond&unidade.rds")
 muni <- geobr::read_municipality(code_muni = "all", year=2010)
 
-counter <- 0
+
 identif <- function(item){
   #identificar municípios de destinos e origens
-  dest <- item$destinations %>% st_as_sf(coords = c('lon', 'lat'), crs = st_crs(area)) %>% st_join(area)
+  dest <- item$destinations #%>% st_as_sf(coords = c('lon', 'lat'), crs = st_crs(area)) %>% st_join(area)
   sour <- item$sources %>% st_as_sf(coords = c('lon', 'lat'), crs = st_crs(muni)) %>% st_join(muni)
   time <- item$durations
   dist <- item$distances
   #corrigir nomes de destinos e origens nas tabelas de tempo e distância de viagem
-  colnames(time) <- dest$Cod_Area_Pond
+  colnames(time) <- (area %>% sf::st_drop_geometry())$Cod_Area_Pond
   rownames(time) <- sour$code_muni
-  colnames(dist) <- dest$Cod_Area_Pond
+  colnames(dist) <- (area %>% sf::st_drop_geometry())$Cod_Area_Pond
   rownames(dist) <- sour$code_muni
   
   #criar novo item, corrigido
@@ -28,20 +28,18 @@ identif <- function(item){
   newItem$durations <- time
   newItem$distances <- dist      
   
-  counter <<- counter+1
-  cat("Item no. ", counter, "/", length(travels), "\n", sep="", collapse="")  
   return(newItem)
 }
 
 travelsFinal <-lapply(travels, identif) 
-rm(counter, travels)
+rm(travels)
 
 #arquivo tempo_municipios_corrigido
 #travelsFinal <- readRDS(file.choose())
 
 #junção de lista para toda a tabela
 allTimes <- travelsFinal %>% 
-  lapply(., "[[", "durations") %>% 
+  lapply(., "[[", "durations") #%>% 
   lapply(., function(x){
     df <- as.data.frame(x)
     df$origens <- rownames(x)
@@ -57,7 +55,7 @@ timesDists <- travelsFinal %>%
     df$origens <- rownames(x)
     return(df)
   }) %>% 
-  do.call(bind_rows, .) %>% 
+  do.call(bind_rows, .) #%>% 
   gather(key = 'destinos', value = 'distância', -origens) %>% 
   full_join(allTimes, by = c("origens", "destinos")) %>% 
   rename(origem = origens, destino = destinos) %>% 
